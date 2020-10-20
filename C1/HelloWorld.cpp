f46bfe970f3a972944137e9658fc96b8700e155e
ritsukishuto <rtk410s@gmail.com>
2020-10-20 14:21:45 +0900

    master

---
+++ C1/HelloWorld.cpp
@@ -0,0 +1,10 @@
+// Created by RitsukiShuto on 2020/10/20.
+// HelloWorld.cpp
+//
+#include<iostream>
+
+int main(int argc, char *argv[]){
+    std::cout << "Hello, World !!" << std::endl;
+
+    return 0;
+}
